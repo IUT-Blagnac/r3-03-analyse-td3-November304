62f4d89aebdefa0355a32453de77c2866ae3367e
November304 <nathan.p3pro@gmail.com>
2023-09-21 12:24:06 +0200

    Push

--- tp3/src/main/java/com/nathan/App.java
+++ tp3/src/main/java/com/nathan/App.java
@@ -1,7 +1,11 @@
 package com.nathan;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public static String hello() {
+        return  "Hello World!" ;
     }
 }
