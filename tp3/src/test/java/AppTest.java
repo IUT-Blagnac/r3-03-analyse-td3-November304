16f134f55e29bd5fd6f0768bef76db6a1ec929cc
November304 <nathan.p3pro@gmail.com>
2023-09-21 11:56:38 +0200

    Création de mon projet et modif du readme

---
+++ tp3/src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+/**
+ * Class de tests unitaire pour la classe main
+ */
+public class AppTest extends TestCase {
+    
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+}
